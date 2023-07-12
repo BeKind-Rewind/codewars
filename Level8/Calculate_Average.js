32b3eadcbecb05df2a3795bb4145619489c54d15
Amanda Perry <challenge641@gmail.com>
2023-07-12 10:15:10 -0500

    adding level 8 Calc average array

--- Level8/Calculate_Average.js
+++ Level8/Calculate_Average.js
@@ -2,3 +2,19 @@
 
 // Note: Empty arrays should return 0.
 
+// 
+
+function findAverage(array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return +0;
+  }
+  else {
+    for (i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    let average = sum / array.length;
+    return average
+  }
+
+}
