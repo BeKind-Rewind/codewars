932d5fb54fbb2f72b677e40e658acb6d5f1ebb0c
Amanda Perry <challenge641@gmail.com>
2023-07-19 12:40:31 -0500

    level 8 kata solution added

--- Level8/Total_Points.js
+++ Level8/Total_Points.js
@@ -20,4 +20,29 @@
 
 // MY CODE
 
-// OTHER
+function points(games) {
+  // initialize a variable bucket to sum as we go
+  let championshipPoints = 0;
+  // for loop to iterate through all the games
+  for (let i = 0; i < games.length; i++) {
+    // initialize a place to split the string into its charachters to compare scores and find win, lode or draw
+    let x = games[i].split(';')
+    // if conditional to test against, x[0] is "our team" and x[1] is the competition
+    if (x[0] > x[1]) {
+      // we won, add 3 points to championshipPoints
+      championshipPoints += 3
+    } else if (x[0] == x[1]) {
+      // we tied, add 1 point to championshiopPoints
+      championshipPoints += 1;
+    } else
+      // we lost and no points are added
+      championshipPoints = championshipPoints;
+  }
+  return championshipPoints;
+}
+
+// OTHER, common to use reduce()
+
+const points = games => games.reduce((output, current) => {
+  return output += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0;
+}, 0)
