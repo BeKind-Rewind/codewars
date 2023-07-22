47a849609e62646a6ec2d52a7ef04d23a1b3a00f
Amanda Perry <challenge641@gmail.com>
2023-07-22 10:26:29 -0500

    Kata level 8 Convert Number of Reversed Array Digits solution - Array.from() and unshift method

--- Level8/Convert_no_of_rev_arr_digits.js
+++ Level8/Convert_no_of_rev_arr_digits.js
@@ -8,6 +8,12 @@
 
 // MY CODE
 
+let digits = Array.from(String(n), Number); // Array.from creates an array from the first arg and maps with the second arg
+let reversedDigits = [];
+for (let i = 0; i < digits.length; i++) {
+  reversedDigits.unshift(digits[i]); // .unshift() adds elements from the arg to the beginning of the chained array
+}
 
+return reversedDigits;
 
 // OTHERS
