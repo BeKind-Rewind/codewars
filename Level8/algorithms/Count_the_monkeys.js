1bad22d29094e11f5187d6c43686ef57accc51f7
Amanda Perry <challenge641@gmail.com>
2023-09-03 13:33:11 -0500

    counting monkeys solution using a for loop

--- Level8/algorithms/Count_the_monkeys.js
+++ Level8/algorithms/Count_the_monkeys.js
@@ -10,3 +10,11 @@
 // ARRAYSFUNDAMENTALSLISTSALGORITHMS
 
 // MY CODE:
+
+function monkeyCount(n) {
+  let countingMonkeys = [];
+  for (let i = 1; i <= n; i++) {
+    countingMonkeys.push(i);
+  }
+  return countingMonkeys;
+}
