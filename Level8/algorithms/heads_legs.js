88445ef61404b907ee435a02bea5f5ed221632cf
Amanda Perry <challenge641@gmail.com>
2023-09-19 11:48:23 -0500

    heads and legs solution

--- Level8/algorithms/heads_legs.js
+++ Level8/algorithms/heads_legs.js
@@ -35,3 +35,12 @@
 
 // MY CODE:
 
+function animals(heads, legs) {
+  const cows = legs / 2 - heads;
+  const chickens = heads - cows;
+
+  if (legs & 1 || chickens > heads || cows > heads)
+    return "No solutions";
+  else
+    return [chickens, cows];
+}
