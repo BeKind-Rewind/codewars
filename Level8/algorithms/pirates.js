0d132eb8875dfa100b5c90e2b9d39bce7b9b21d8
Amanda Perry <challenge641@gmail.com>
2023-09-28 14:27:43 -0500

    solution

--- Level8/algorithms/pirates.js
+++ Level8/algorithms/pirates.js
@@ -27,3 +27,12 @@
 
 // MY CODE:
 
+const cannonsReady = (gunners) => {
+  let ready = true;
+  for (let i in gunners) {
+    if (gunners[i] === 'nay') {
+      ready = false;
+    }
+  }
+  return ready ? 'Fire!' : 'Shiver me timbers!';
+}
