b3178db4cc12a9d4d2d371c2dd72f0d96b19ba66
Amanda Perry <challenge641@gmail.com>
2024-02-14 12:28:58 -0600

    debugging and OOP practice

---
+++ Level8/OOP/broken_counter.js
@@ -0,0 +1,46 @@
+// Our counter prototype is broken.Can you spot, what's wrong here?
+
+// DEBUGGINGOBJECT - ORIENTED PROGRAMMING
+
+// GIVEN
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase() {
+  this.value++;
+};
+
+Counter.prototype.getValue() {
+  return this.value;
+};
+
+Counter.prototype.reset() {
+  this.value = 0;
+};
+
+
+// SAMPLE TESTS
+const Test = require('@codewars/test-compat');
+
+describe('test counter', function () {
+  var counter = new Counter();
+
+  it('initialize', function () {
+    Test.assertEquals(counter.getValue(), 0, 'Initial counter value must be 0');
+  });
+
+  it('increase', function () {
+    counter.increase();
+    Test.assertEquals(counter.getValue(), 1, 'Counter value must be increased');
+  });
+
+  it('reset', function () {
+    counter.reset();
+    Test.assertEquals(counter.getValue(), 0, 'Counter value must be 0 after reset');
+  });
+});
+
+
+// SOLUTION
