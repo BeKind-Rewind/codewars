256ebf5e5cadd90eaed58b4cebef327772649b9e
Amanda Perry <challenge641@gmail.com>
2024-02-18 14:41:19 -0600

    adding logs

---
+++ Level8/algorithms/easy_logs.js
@@ -0,0 +1,11 @@
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+// ALGORITHMS
+
+// GIVEN
+
+// SOLUTION
+
+function logs(x, a, b) {
+  return (Math.log(a * b) / Math.log(x));
+}
