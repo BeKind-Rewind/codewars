cb69bcdcf28bd281bdffdc0d53ac47e02b5eae58
Amanda Perry <challenge641@gmail.com>
2024-02-17 21:56:02 -0600

    Prototype method for best practices

--- Level8/OOP/barking_mad.js
+++ Level8/OOP/barking_mad.js
@@ -8,5 +8,50 @@
 
 // GIVEN
 
-// SOLUTION
+function Dog(breed) {
+  this.breed = breed;
+}
 
+let snoopy = new Dog('Beagle');
+
+let scoobydoo = new Dog('Great Dane');
+
+// SOLUTION 1 - it WORKS but is not best practice because it creates a new method each instance, takes more memory, no inheritance utilized
+
+function Dog(breed) {
+  this.breed = breed;
+  this.bark = function () { return "Woof" }
+}
+
+let snoopy = new Dog('Beagle');
+
+let scoobydoo = new Dog('Great Dane');
+
+// SOLUTION 2
+
+class Dog {
+  constructor() {
+    this.breed = breed;
+  }
+
+  bark() {
+    return "Woof"
+  }
+}
+
+let snoopy = new Dog('Beagle');
+let scoobydoo = new Dog('Great Dane');
+
+
+
+// SOLUTION 3 - the best solution because of prototype method is what was called for
+function Dog(breed) {
+  this.breed = breed;
+}
+
+Dog.prototype.bark = function () {
+  return "Woof";
+}
+
+let snoopy = new Dog('Beagle');
+let scoobydoo = new Dog('Great Dane');
