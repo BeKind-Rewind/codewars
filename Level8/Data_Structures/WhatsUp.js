ec0ef5a494604e1dfa1f900f5871170ff4b39982
Amanda Perry <challenge641@gmail.com>
2024-02-23 13:46:02 -0600

    simple but difficult solution for my brain

--- Level8/Data_Structures/WhatsUp.js
+++ Level8/Data_Structures/WhatsUp.js
@@ -12,3 +12,11 @@
 
 // SOLUTION
 
+function nextItem(xs, item) {
+  var found = false;
+  for (var x of xs) {
+    if (found) return x;
+    if (x == item) found = true;
+  }
+  return undefined;
+}
