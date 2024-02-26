116a14ad615c57b5ceef0075696c0b330ff154e8
Amanda Perry <challenge641@gmail.com>
2024-02-26 15:09:28 -0600

    days in year solution

--- Level8/fundamentals/days_in_year.js
+++ Level8/fundamentals/days_in_year.js
@@ -16,3 +16,11 @@
 // DATE TIMEFUNDAMENTALS
 
 // SOLUTION
+
+function yearDays(year) {
+  let days = (
+    year % 400 === 0 ? 366 :
+      year % 100 === 0 ? 365 :
+        year % 4 === 0 ? 366 : 365)
+  return `${year} has ${days} days`
+}
