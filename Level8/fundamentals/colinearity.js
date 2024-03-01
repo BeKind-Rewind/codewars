4880b8e3647aed11d0be53a34d3e2d0e941531cd
Amanda Perry <challenge641@gmail.com>
2024-03-01 14:01:53 -0600

    solution

--- Level8/fundamentals/colinearity.js
+++ Level8/fundamentals/colinearity.js
@@ -34,3 +34,5 @@
 // FUNDAMENTALSGEOMETRYMATHEMATICSDATA SCIENCE
 
 // SOLUTION
+
+const collinearity = (x1, y1, x2, y2) => x1 === 0 && y1 === 0 || x2 === 0 && y2 === 0 ? true : (y1 / x1) === (y2 / x2) ? true : false
