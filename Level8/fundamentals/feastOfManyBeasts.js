1f5d6d08a513479192f6c0aa6943e00ff7cf75b4
Amanda Perry <challenge641@gmail.com>
2024-03-07 21:58:10 -0500

    two solutions

--- Level8/fundamentals/feastOfManyBeasts.js
+++ Level8/fundamentals/feastOfManyBeasts.js
@@ -16,3 +16,8 @@
 
 // SOLUTION
 
+function feast(beast, dish) {
+  //   return beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length -1) === dish.charAt(dish.length -1)
+  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+
+}
