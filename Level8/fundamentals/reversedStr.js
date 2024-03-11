8b64947ab3b285278cdec8315f8847670281caef
Amanda Perry <challenge641@gmail.com>
2024-03-11 12:04:59 -0500

    rerefactored

--- Level8/fundamentals/reversedStr.js
+++ Level8/fundamentals/reversedStr.js
@@ -13,3 +13,13 @@ function solution(str) {
 // refactored
 
 const solution = str => str.split('').reverse().join('');
+
+// re-refactored
+
+function solution(str) {
+  let revStr = ""
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str[i]
+  }
+  return revStr
+}
