24f5fc10456ae5794dfb2b8c13e6381701c9ca80
Amanda Perry <challenge641@gmail.com>
2024-03-11 12:12:57 -0500

    rerefactored recursive

--- Level8/fundamentals/reversedStr.js
+++ Level8/fundamentals/reversedStr.js
@@ -23,3 +23,17 @@ function solution(str) {
   }
   return revStr
 }
+
+// recursive!
+function solution(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
+}
+
+// refactor
+
+function reverseString(str) {
+  return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+}
