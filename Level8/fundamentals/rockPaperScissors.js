7c1c6f3c27ff4145bc81ff44ac5daa756b868495
Amanda Perry <challenge641@gmail.com>
2024-03-13 21:50:33 -0500

    solution with ternary statements

--- Level8/fundamentals/rockPaperScissors.js
+++ Level8/fundamentals/rockPaperScissors.js
@@ -9,4 +9,14 @@
 
 // SOLUTIONS
 
-
+const rps = (p1, p2) => {
+  let result =
+    p1 === "scissors" && p2 === "paper" ||
+      p1 === "paper" && p2 === "rock" ||
+      p1 === "rock" && p2 === "scissors" ? "Player 1 won!" :
+      p2 === "scissors" && p1 === "paper" ||
+        p2 === "paper" && p1 === "rock" ||
+        p2 === "rock" && p1 === "scissors" ? "Player 2 won!" :
+        "Draw!"
+  return result
+};
