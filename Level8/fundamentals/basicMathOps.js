7d4885d70979d4462941720310db41ec9a34a779
Amanda Perry <challenge641@gmail.com>
2024-03-15 15:14:57 -0500

    switch case solution

--- Level8/fundamentals/basicMathOps.js
+++ Level8/fundamentals/basicMathOps.js
@@ -22,3 +22,18 @@ function basicOp(operation, value1, value2) {
   return result;
 }
 
+// switch case
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default: return 'Operation must be one of + - * /';
+  }
+}
