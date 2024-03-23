e3a06defa73ec042eb69b07671b92cca37e34715
Amanda Perry <challenge641@gmail.com>
2024-03-23 10:46:13 -0500

    refactored more efficient solution

--- Level7/Fundamentals/catYrDogYr.js
+++ Level7/Fundamentals/catYrDogYr.js
@@ -38,3 +38,12 @@ var ownedCatAndDog = function (catYears, dogYears) {
 
   return [ownedCat, ownedDog];
 }
+
+// more efficient solution
+
+const ownedCatAndDog = (...pets) => pets.map((petYears, i) => {
+  if (petYears < 15) return 0;
+  if (petYears < 24) return 1;
+
+  return 2 + Math.floor((petYears - 24) / (4 + i));
+});
