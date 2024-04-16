7673a0d44a62135f234a5af454e581491225caeb
Amanda Perry <challenge641@gmail.com>
2024-04-16 13:14:47 -0500

    level 7 kata, array sum 2 lowest pos int solution slower

--- Level8/arrays/Sum_of_two_low_pos_int.js
+++ Level8/arrays/Sum_of_two_low_pos_int.js
@@ -26,3 +26,9 @@ function sumTwoSmallestNumbers(numbers) {
   return min + secondMin;
 }
 
+// much slower time complexity 
+
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
