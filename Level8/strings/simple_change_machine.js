071333839c55094f2c0433cc8fb90470091e6736
Amanda Perry <challenge641@gmail.com>
2024-04-30 11:07:49 -0500

    level 8 kata, strings, added simple change machine solution switch case

--- Level8/strings/simple_change_machine.js
+++ Level8/strings/simple_change_machine.js
@@ -19,3 +19,25 @@ function changeMe(moneyIn){
   if(moneyIn === "50p") return "20p 20p 10p";
   if(moneyIn === "20p") return "10p 10p";
 }
+
+// switch case solution
+
+function changeMe(moneyIn){
+  switch (moneyIn) {
+    case '£5':
+      var change = Array(25).fill('20p');
+      return change.join(' ');
+    case '£2':
+      var change = Array(10).fill('20p');
+      return change.join(' ');
+    case '£1':
+      var change = Array(5).fill('20p');
+      return change.join(' ');
+    case '50p':
+      return '20p 20p 10p';
+    case '20p':
+      return '10p 10p';
+    default:
+      return moneyIn;
+  }
+}
