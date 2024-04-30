3af6bb8710076e4027f2ce19955a1ef17f6d4f05
Amanda Perry <challenge641@gmail.com>
2024-04-30 11:06:22 -0500

    level 8 kata, strings, added simple change machine solution basic

--- Level8/strings/simple_change_machine.js
+++ Level8/strings/simple_change_machine.js
@@ -11,3 +11,11 @@
 
 // SOLUTION
 
+function changeMe(moneyIn){
+  if(moneyIn != "£5" && moneyIn != "£2" && moneyIn != "£1" && moneyIn != "50p" && moneyIn != "20p") return moneyIn;
+  if(moneyIn === "£5") return "20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p";
+  if(moneyIn === "£2") return "20p 20p 20p 20p 20p 20p 20p 20p 20p 20p";
+  if(moneyIn === "£1") return "20p 20p 20p 20p 20p";
+  if(moneyIn === "50p") return "20p 20p 10p";
+  if(moneyIn === "20p") return "10p 10p";
+}
