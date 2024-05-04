a0bf4ef490b74fc3d012ebfca905bd7f9cc817d3
Amanda Perry <challenge641@gmail.com>
2024-05-04 15:46:46 -0500

    level 7 kata, strings, lottery machine solution

--- Level7/strings/lottery_machine.js
+++ Level7/strings/lottery_machine.js
@@ -9,3 +9,24 @@
 
 // SOLUTION
 
+function lottery(str){
+  let update =""
+  for (let i = 0; i < str.length ; i++){
+    if(str[i] === '0' ||
+       str[i] === '1' ||
+       str[i] === '2' ||
+       str[i] === '3' ||
+       str[i] === '4' ||
+       str[i] === '5' ||
+       str[i] === '6' ||
+       str[i] === '7' ||
+       str[i] === '8' ||
+       str[i] === '9'){
+      if (!update.includes(str[i])){
+        update += str[i]
+        }
+    } 
+  }
+ return update ? update : "One more run!";
+}
+
