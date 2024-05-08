9d2d8667ee6bf75e90613f5b71fab99a940393a8
Amanda Perry <challenge641@gmail.com>
2024-05-08 17:23:02 -0500

    level 7 kata, strings, Find the longest gap solution

--- Level7/strings/find_the_longest_gap.js
+++ Level7/strings/find_the_longest_gap.js
@@ -12,3 +12,28 @@
 
 // SOLUTION
 
+function gap(num) {
+  
+  // #1 - convert num to binary
+  let binary = num.toString(2)
+  
+  // #2 - Find indexes of any ones
+  // declare array to hold indexes of 1s; 
+  let oneIndex = ['0'] // position [0] will always be 1
+  for (let i = 1 ; i < binary.length ; i ++){
+      if (binary[i]=='1') {
+          oneIndex.push(i);
+      } 
+  }
+
+  // #3 - Compare indexes to find gaps
+  let gapArr = [] // length of array is number of gaps
+  for (let j = 0; j < oneIndex.length ; j++){
+    if (oneIndex[j+1] - oneIndex[j] > 1){
+      gapArr.push(oneIndex[j+1] - oneIndex[j] - 1) // finds size of gap
+    }
+  }
+
+  // #4 - if no gaps are found return 0, else return the largest gap value from gapArr
+  return gapArr.length < 1 ? 0 : Math.max(...gapArr)
+}
