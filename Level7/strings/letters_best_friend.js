766debe7c2c8a4a6b50a250ba23b23289fd0e125
Amanda Perry <challenge641@gmail.com>
2024-05-12 11:25:03 -0500

    level 7 kata strings, A Letter's Best Friend added refactored solution

--- Level7/strings/letters_best_friend.js
+++ Level7/strings/letters_best_friend.js
@@ -40,3 +40,12 @@ function bestFriend(txt, a, b) {
   }
   return true
 }
+
+// REFACTOR
+
+function bestFriend(txt, a, b) {
+  for(let i = 0; i < txt.length; i++){
+    if(txt[i] === a && txt[i + 1] !== b) return false 
+  }
+  return true
+}
