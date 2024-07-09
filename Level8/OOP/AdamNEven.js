8d462f829eb3b01567874909afb655fbff9de807
Amanda Perry <challenge641@gmail.com>
2024-07-09 13:31:52 -0500

    level 8kyu, Adam and Eve solution

--- Level8/OOP/AdamNEven.js
+++ Level8/OOP/AdamNEven.js
@@ -13,3 +13,16 @@
 
 // SOLUTION
 
+class God{
+  /**
+   * @returns Human[]
+   */
+    static create(){
+      return [new Man, new Woman];
+    }
+  }
+  class Human{}
+  class Man extends Human{}
+  class Woman extends Human{}
+
+  
